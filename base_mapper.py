399cdb8762b9169ea8580de882353067efaeb051
Sam <109683132+kernelsam@users.noreply.github.com>
2025-06-16 13:21:52 -0700

    senzing-factory/build-resources#185 add spellcheck (#37)

--- base_mapper.py
+++ base_mapper.py
@@ -8,41 +8,45 @@ import time
 import signal
 import random
 
-#=========================
-class base_library():
 
-    #----------------------------------------
-    def __init__(self, mappingStandardsFile, isoCountrySize = 3):
+# =========================
+class base_library:
+
+    # ----------------------------------------
+    def __init__(self, mappingStandardsFile, isoCountrySize=3):
         self.initialized = True
         self.statPack = {}
 
         if not os.path.exists(mappingStandardsFile):
-            print('')
-            print('File %s is missing!' % mappingStandardsFile)
-            print('')
+            print("")
+            print("File %s is missing!" % mappingStandardsFile)
+            print("")
             self.initialized = False
             return
 
-        try: self.mapping_standards = json.load(open(mappingStandardsFile,'r', encoding='latin-1'))
+        try:
+            self.mapping_standards = json.load(
+                open(mappingStandardsFile, "r", encoding="latin-1")
+            )
         except json.decoder.JSONDecodeError as err:
-            print('')
-            print('JSON error %s in %s' % (err, mappingStandardsFile))
-            print('')
+            print("")
+            print("JSON error %s in %s" % (err, mappingStandardsFile))
+            print("")
             self.initialized = False
             return
 
-        if 'ORGANIZATION_TOKENS' not in self.mapping_standards:
-            self.mapping_standards['ORGANIZATION_TOKENS'] = {}
-        if 'PERSON_TOKENS' not in self.mapping_standards:
-            self.mapping_standards['PERSON_TOKENS'] = {}
-        if 'STATE_CODES' not in self.mapping_standards:
-            self.mapping_standards['STATE_CODES'] = {}
-        if 'COUNTRY_CODES' not in self.mapping_standards:
-            self.mapping_standards['COUNTRY_CODES'] = {}
-        if 'BAD_VALUES' not in self.mapping_standards:
-            self.mapping_standards['BAD_VALUES'] = {}
+        if "ORGANIZATION_TOKENS" not in self.mapping_standards:
+            self.mapping_standards["ORGANIZATION_TOKENS"] = {}
+        if "PERSON_TOKENS" not in self.mapping_standards:
+            self.mapping_standards["PERSON_TOKENS"] = {}
+        if "STATE_CODES" not in self.mapping_standards:
+            self.mapping_standards["STATE_CODES"] = {}
+        if "COUNTRY_CODES" not in self.mapping_standards:
+            self.mapping_standards["COUNTRY_CODES"] = {}
+        if "BAD_VALUES" not in self.mapping_standards:
+            self.mapping_standards["BAD_VALUES"] = {}
 
-        #--supported date formats
+        # --supported date formats
         self.dateFormats = []
         self.dateFormats.append("%Y-%m-%d")
         self.dateFormats.append("%m/%d/%Y")
@@ -63,104 +67,115 @@ class base_library():
         self.dateFormats.append("%d-%b")
         self.dateFormats.append("%d/%b")
 
-        #--set iso country code size 
+        # --set iso country code size
         if isoCountrySize not in (2, 3):
-            print('')
-            print('The ISO Country size must be 2 or 3.')
-            print('')
+            print("")
+            print("The ISO Country size must be 2 or 3.")
+            print("")
             self.initialized = False
-        self.isoCountrySize = 'ISO' + str(isoCountrySize)
-    
-    #----------------------------------------
-    def formatDate(self, dateString, outputFormat = None):
+        self.isoCountrySize = "ISO" + str(isoCountrySize)
+
+    # ----------------------------------------
+    def formatDate(self, dateString, outputFormat=None):
         for dateFormat in self.dateFormats:
-            try: dateValue = datetime.strptime(dateString, dateFormat)
-            except: pass
-            else: 
+            try:
+                dateValue = datetime.strptime(dateString, dateFormat)
+            except:
+                pass
+            else:
                 if not outputFormat:
                     if len(dateString) == 4:
-                        outputFormat = '%Y'
-                    elif len(dateString) in (5,6):
-                        outputFormat = '%m-%d'
-                    elif len(dateString) in (7,8):
-                        outputFormat = '%Y-%m'
+                        outputFormat = "%Y"
+                    elif len(dateString) in (5, 6):
+                        outputFormat = "%m-%d"
+                    elif len(dateString) in (7, 8):
+                        outputFormat = "%Y-%m"
                     else:
-                        outputFormat = '%Y-%m-%d'
+                        outputFormat = "%Y-%m-%d"
                 return datetime.strftime(dateValue, outputFormat)
         return None
 
-    #---------------------------------------
+    # ---------------------------------------
     def isoCountryCode(self, countryString):
-        countryString = countryString.replace('.', '').upper()
-        if countryString in self.mapping_standards['COUNTRY_CODES']:
-            return self.mapping_standards['COUNTRY_CODES'][countryString.upper()][self.isoCountrySize]
-        elif ',' in countryString:
-            countryString = countryString[countryString.rfind(',')+1:].strip()
-            if countryString in self.mapping_standards['COUNTRY_CODES']:
-                return self.mapping_standards['COUNTRY_CODES'][countryString.upper()][self.isoCountrySize]
+        countryString = countryString.replace(".", "").upper()
+        if countryString in self.mapping_standards["COUNTRY_CODES"]:
+            return self.mapping_standards["COUNTRY_CODES"][countryString.upper()][
+                self.isoCountrySize
+            ]
+        elif "," in countryString:
+            countryString = countryString[countryString.rfind(",") + 1 :].strip()
+            if countryString in self.mapping_standards["COUNTRY_CODES"]:
+                return self.mapping_standards["COUNTRY_CODES"][countryString.upper()][
+                    self.isoCountrySize
+                ]
 
-    #---------------------------------------
+    # ---------------------------------------
     def isoStateCode(self, stateString):
-        if stateString.upper() in self.mapping_standards['STATE_CODES']:
-            return self.mapping_standards['STATE_CODES'][stateString.upper()]
+        if stateString.upper() in self.mapping_standards["STATE_CODES"]:
+            return self.mapping_standards["STATE_CODES"][stateString.upper()]
 
-    #-----------------------------------
+    # -----------------------------------
     def isCompanyName(self, nameString):
         if nameString:
-            for token in nameString.lower().replace('.',' ').replace(',',' ').split():
-                if token.upper() in self.mapping_standards['ORGANIZATION_TOKENS']:
+            for token in nameString.lower().replace(".", " ").replace(",", " ").split():
+                if token.upper() in self.mapping_standards["ORGANIZATION_TOKENS"]:
                     return True
         return False
 
-    #----------------------------------------
+    # ----------------------------------------
     def dictKeysUpper(self, jsonDataIn):
-        return {k.upper():v for k,v in jsonDataIn.items()}
+        return {k.upper(): v for k, v in jsonDataIn.items()}
 
-    #----------------------------------------
-    def updateStat(self, cat1, cat2, example = None):
+    # ----------------------------------------
+    def updateStat(self, cat1, cat2, example=None):
         if cat1 not in self.statPack:
             self.statPack[cat1] = {}
         if cat2 not in self.statPack[cat1]:
             self.statPack[cat1][cat2] = {}
-            self.statPack[cat1][cat2]['count'] = 0
+            self.statPack[cat1][cat2]["count"] = 0
 
-        self.statPack[cat1][cat2]['count'] += 1
+        self.statPack[cat1][cat2]["count"] += 1
         if example:
-            if 'examples' not in self.statPack[cat1][cat2]:
-                self.statPack[cat1][cat2]['examples'] = []
-            if example not in self.statPack[cat1][cat2]['examples']:
-                if len(self.statPack[cat1][cat2]['examples']) < 5:
-                    self.statPack[cat1][cat2]['examples'].append(example)
+            if "examples" not in self.statPack[cat1][cat2]:
+                self.statPack[cat1][cat2]["examples"] = []
+            if example not in self.statPack[cat1][cat2]["examples"]:
+                if len(self.statPack[cat1][cat2]["examples"]) < 5:
+                    self.statPack[cat1][cat2]["examples"].append(example)
                 else:
-                    randomSampleI = random.randint(2,4)
-                    self.statPack[cat1][cat2]['examples'][randomSampleI] = example
+                    randomSampleI = random.randint(2, 4)
+                    self.statPack[cat1][cat2]["examples"][randomSampleI] = example
         return
 
-#----------------------------------------
+
+# ----------------------------------------
 def attrInList(jsonAttr, attrList):
     for attrName in attrList:
         if attrName in jsonAttr:
             return True
     return False
 
-#----------------------------------------
-def pause(question='PRESS ENTER TO CONTINUE ...'):
-    """ pause for debug purposes """
-    try: response = input(question)
+
+# ----------------------------------------
+def pause(question="PRESS ENTER TO CONTINUE ..."):
+    """pause for debug purposes"""
+    try:
+        response = input(question)
     except KeyboardInterrupt:
         response = None
         global shutDown
         shutDown = True
     return response
 
-#----------------------------------------
+
+# ----------------------------------------
 def signal_handler(signal, frame):
-    print('USER INTERUPT! Shutting down ... (please wait)')
+    print("USER INTERRUPT! Shutting down ... (please wait)")
     global shutDown
     shutDown = True
     return
 
-#----------------------------------------
+
+# ----------------------------------------
 if __name__ == "__main__":
     appPath = os.path.dirname(os.path.abspath(sys.argv[0]))
 
@@ -170,12 +185,11 @@ if __name__ == "__main__":
     procStartTime = time.time()
     progressInterval = 10000
 
-    #--test the instance
-    baseLibrary = base_library(appPath + os.path.sep + 'base_variants.json')
+    # --test the instance
+    baseLibrary = base_library(appPath + os.path.sep + "base_variants.json")
     if baseLibrary.initialized:
-        print('')
-        print('successfully initialized!')
-        print('')
+        print("")
+        print("successfully initialized!")
+        print("")
 
     sys.exit()
-
